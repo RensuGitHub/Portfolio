5e5b8861b81e6b6154820752e2dcf11400a0a025
Garvs Kantohan <93835334+RensuGitHub@users.noreply.github.com>
2025-05-23 00:34:39 +0800

    Setup + Dependencies
    Initialized React + TS Template
    [+] Added TailwindCSS
    [+] Shadcn UI Configuration

---
+++ my-portfolio/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold text-black">.-.. --- .- -.. .. -. --.</h1>
+    </div>
+  );
+}
+export default App;
